9b6ea7af6c8f5bb0022921cf650246ed3b479e04
Vaibhav Giri <girivaibhav918@gmail.com>
2025-10-23 22:50:20 +0530

    ETL demo2

--- extract.py
+++ extract.py
@@ -3,9 +3,9 @@ import pandas as pd
 print("Extract data from MYSQL")
 
 data = {
-    "emp_id" = [1,2,3],
-    "emp_name" = ["Vaibhav","Kiran","Avnish"],
-    "salary" = [10000,20000,30000] }
+    "emp_id" : [1,2,3],
+    "emp_name" : ["Vaibhav","Kiran","Avnish"],
+    "salary" : [10000,20000,30000]}
 
 df = pd.DataFrame(data)
 print(df)
